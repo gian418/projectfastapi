048d566e08d6337c9db69d0755c60fd10e0bdceb
Giancarlo Haack Albano <gian418@gmail.com>
2025-06-04 14:14:27 -0300

    Iniciando o projeto

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#para rodar o nosso codigo executar uvicorn main:app --reload
-#pip3 install fastapi uvicorn sqlalchemy 'passlib[bcrypt]' 'python-jose[cryptography]' python-dotenv python-multipart
 from fastapi import FastAPI
 
 app = FastAPI()
